fd07955eef875df9534795bbd7cb7f035acb3ba3
vishu000-ops <vpvivekpanchal1991vp@gmail.com>
2026-09-21 18:04:06 +0530

    query parameters

--- fastapienv/books.py
+++ fastapienv/books.py
@@ -14,6 +14,16 @@ app = FastAPI()
 async def read_books():
     return BOOKS
 
-@app.get("/books/{dynamic_param}")
-async def read_all_books(dynamic_param):
-    return {'dynamic_param' : dynamic_param}
+@app.get("/books/{book_title}")
+async def read_book(book_title : str):
+    for book in BOOKS:
+        if book.get('title').casefold() == book_title.casefold():
+            return book
+
+@app.get("/books/")
+async def read_category_by_query(category : str):
+    book_to_return = []
+    for book in BOOKS:
+        if book.get('category').casefold() == category.casefold():
+            book_to_return.append(book)
+    return book_to_return
