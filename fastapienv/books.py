81e084759495ce797465d769a9884c0a0a99a7f2
vishu000-ops <vpvivekpanchal1991vp@gmail.com>
2026-09-22 22:39:29 +0530

    put request

--- fastapienv/books.py
+++ fastapienv/books.py
@@ -31,3 +31,9 @@ async def read_category_by_query(category : str):
 @app.post("/books/create_book")
 async def create_book(new_book=Body()):
     BOOKS.append(new_book)
+
+@app.put("/books/update_book")
+async def update_book(updated_book=Body()):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == updated_book.get('title').casefold():
+            BOOKS[i] = updated_book
